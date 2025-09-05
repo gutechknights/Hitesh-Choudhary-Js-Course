3f86c426cd22685f00e467ab32c0d7992a04c830
gutechknights <gu.techknights@gmail.com>
2025-09-05 08:07:35 +0000

    Conversion of data types

---
+++ Basics/class1.js
@@ -0,0 +1,49 @@
+let v = 1  // conversion of data type like this N = 7 to convert into string S = String(N) or to convert in boolean B = Boolean(N);
+let vs =""
+let vs1 = true
+let vs2 = "Akash"
+let valueofv = Boolean(v);
+let valueofvs = Boolean(vs);
+let valueofvs1 = Boolean(vs1);
+let valueofvs2 = Boolean(vs2);
+console.log(v);
+console.log(1);
+console.log(vs);
+console.log(vs1);
+console.log(vs2);
+console.log(Boolean(v));
+console.log(Boolean(vs));
+console.log(Boolean(vs1));
+console.log(Boolean(vs2));
+console.log(typeof v);
+console.log(typeof vs);
+console.log(typeof vs1);
+console.log(typeof vs2);
+console.log(typeof 1);
+
+console.log(typeof Boolean(v));
+console.log(typeof Boolean(vs));
+console.log(typeof Boolean(vs1));
+console.log(typeof Boolean(vs2));
+
+
+/* when you print values and then convert them to boolean or see as boolean what kind of answers you get this are the results you get
+
+1     here we get let = 1
+      this is empty space result we get for let = ""
+true  this is we get for boolean 
+Akash                                                                                    if we convert 1 to boolean it means true, if we convert "" to boolean then it shows 
+                                                                                        false but if we convert "Akash" into boolean we get true
+true
+false
+true
+true
+number
+string
+boolean
+string
+number
+boolean
+boolean
+boolean
+boolean  */
--- Basics/script.js
+++ Basics/script.js
@@ -1,1 +1,12 @@
 console.log("Hello universe");
+
+//code readablilty should be high 
+
+console.log("Ashking") // ; is not neccssary when writing code in one line
+
+let score = "99"
+let Value = Number(score);
+console.log(typeof score)
+console.log(typeof Value)
+console.log(Value)
+console.log(score)
