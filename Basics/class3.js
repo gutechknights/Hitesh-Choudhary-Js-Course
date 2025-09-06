d64ef86be89e9980c9a37b8211859bb88531f773
gutechknights <gu.techknights@gmail.com>
2025-09-06 12:28:45 +0000

    bhejo ise

---
+++ Basics/class3.js
@@ -0,0 +1,27 @@
+const score = 500
+console.log(score)
+
+// const balanca = new Number(300);
+
+// console.log(balanca);                         // number se related maths and methods
+// console.log(balanca.toString());
+// console.log(score.toString());
+
+// console.log(typeof balanca.toString());
+// console.log(typeof score.toString());
+
+// console.log(balanca.toString().length);   // number ko string me change krne ke baad usme string ke method bhi use kr skte hai
+
+
+//..............more methods like precision and standerd
+const otherNumber = 23.9089;
+const otherNumber2 = 123.089;
+const otherNumber3 = 1323.9089;
+console.log(otherNumber.toPrecision(3));
+console.log(otherNumber2.toPrecision(3));
+console.log(otherNumber3.toPrecision(3));
+//................
+// random number ke liye
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
